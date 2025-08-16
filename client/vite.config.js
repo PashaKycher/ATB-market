c4d08e6990ae5a212a3fecd0cb1266d0debb2e3c
Павло Кучерявих <pasha.kycher89@gmail.com>
2025-08-16 12:28:10 +0300

    changed vercel.json (client+server) changed vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Vite config
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   server: {
+    open: true,
+    proxy: {
+      '/api': 'http://localhost:8080'
+    },
     port: 3000, // для локалки, необов'язково
     allowedHosts: ['57e027fe2e08.ngrok-free.app'], // якщо будеш ще через ngrok ганяти
-  },
-  build: {
-    outDir: 'dist', // Vercel очікує саме цю папку
-  },
+  }
 })
