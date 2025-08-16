be9dbfa6ed42e7d7ab01dcd3149a9a33319269d2
Павло Кучерявих <pasha.kycher89@gmail.com>
2025-08-16 12:13:25 +0300

    fix static and node(api) bag

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Vite config
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  server: {
+    port: 3000, // для локалки, необов'язково
+    allowedHosts: ['57e027fe2e08.ngrok-free.app'], // якщо будеш ще через ngrok ганяти
+  },
+  build: {
+    outDir: 'dist', // Vercel очікує саме цю папку
+  },
 })
-
-// server: {
-//   allowedHosts: ['57e027fe2e08.ngrok-free.app'], // твой ngrok-хост
-// },
