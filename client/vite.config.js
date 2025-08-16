915b46f07805413888599784193004da9f744ada
Павло Кучерявих <pasha.kycher89@gmail.com>
2025-08-16 13:38:24 +0300

    changed vercel.json (client+server) changed vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,17 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    open: true,
+    proxy: {
+      '/api': 'http://localhost:8080'
+    },
+    port: 3000, // для локалки, необов'язково
+    allowedHosts: ['57e027fe2e08.ngrok-free.app'], // якщо будеш ще через ngrok ганяти
+  }
 })
-
-// build: {
-//     outDir: 'dist'
-//   },
-//   server: {
-//     open: true,
-//     proxy: {
-//       '/api': 'http://localhost:8080'
-//     },
-//     port: 3000, // для локалки, необов'язково
-//     allowedHosts: ['57e027fe2e08.ngrok-free.app'], // якщо будеш ще через ngrok ганяти
-//   }
